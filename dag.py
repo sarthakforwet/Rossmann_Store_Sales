21aae0d88ba4d00909526a9b9f2a83be584b8812
Sarthak Khandelwal <khandelwal.sar@northeastern.edu>
2024-11-16 17:05:24 -0500

    dag.py: updated dag structure

--- dag.py
+++ dag.py
@@ -14,23 +14,13 @@ default_args = {
     'retries': 0,
 }
 
-dag = DAG('fetch_rossman_data',
+dag = DAG('fetch_rossman',
           default_args=default_args,
           description='Runs an external Python script',
           schedule_interval='@daily',
           catchup=False)
 
 with dag:
-    # setup_kaggle = BashOperator(
-    #     task_id = 'setup_kaggle_credentials',
-    #     bash_command = '''
-    #     mkdir -p /home/airflow/.config
-    #     mkdir -p /home/airflow/.config/kaggle
-    #     cp /home/airflow/gcs/dags/data/kaggle.json /home/airflow/.config/kaggle/
-    #     chmod 600 /home/airflow/.config/kaggle/kaggle.json
-    #     '''
-    # )
-
     run_script_task = BashOperator(
         task_id='run_script',
         bash_command='python /home/airflow/gcs/dags/scripts/extract_and_push_to_gcs.py',
